1d70340c725b34211eed4a63d3a9208d54f42cdb
Nawar Hussein <nawar_hussien2000@yahoo.com>
2022-01-13 22:41:51 -0500

    Toyota

---
+++ JavaProgramming/src/day38_Inheritance/carTask/carTask/Car.java
@@ -0,0 +1,32 @@
+package day38_Inheritance.carTask.carTask;
+
+public class Car {
+    public String brand,model,color;
+    public int year;
+    public double price,miles;
+
+    public Car(String brand, String model, String color, int year, double price, double miles) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.year = year;
+        this.price = price;
+        this.miles = miles;
+    }
+
+
+
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", miles=" + miles +
+                '}';
+    }
+
+
+}
+
