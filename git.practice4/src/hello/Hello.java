7e697c0596f74f5ef5f398e1a8433974f9fc08ca
Nawar Hussein <nawar_hussien2000@yahoo.com>
2022-01-24 10:39:32 -0500

    doing final work on hello feature

--- git.practice4/src/hello/Hello.java
+++ git.practice4/src/hello/Hello.java
@@ -4,6 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world");
 
-
+        System.out.println("B 25 learning git and github");
     }
 }
